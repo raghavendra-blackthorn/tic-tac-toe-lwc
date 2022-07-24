ffec1e705c00ced56cd0002ccbae7e11ec905869
VijayKumar-KR <vijay.kr@rubrik.com>
2022-07-23 23:06:55 -0700

    displayed carousal for three cards

--- force-app/main/default/lwc/carousel/carousel.js
+++ force-app/main/default/lwc/carousel/carousel.js
@@ -1,3 +1,10 @@
 import { LightningElement } from 'lwc';
+import sf_blog_img from '@salesforce/resourceUrl/sf_blog_img';
+import number_game_img from '@salesforce/resourceUrl/number_game_img';
+import tic_tac_toe_img from '@salesforce/resourceUrl/tic_tac_toe_img';
 
-export default class Carousel extends LightningElement {}
+export default class Carousel extends LightningElement {
+    sf_blog_img = sf_blog_img;
+    number_game_img = number_game_img;
+    tic_tac_toe_img = tic_tac_toe_img;
+}
